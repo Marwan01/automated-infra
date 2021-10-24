0125c15906450be79ceaa25537fcf1484b1a84cb
Marouen Helali <marwanest@live.fr>
2021-10-24 15:08:24 -0700

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -4,7 +4,7 @@ provider "google" {
 }
 
 resource "google_storage_bucket_access_control" "public_rule" {
-  bucket = google_storage_bucket.bucket.name
+  bucket = google_storage_bucket.static_site.name
   role   = "READER"
   entity = "allUsers"
 }
