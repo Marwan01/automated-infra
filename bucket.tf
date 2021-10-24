fda248627413a4bd27ef38199e1d893f9ec20fad
Marouen Helali <marwanest@live.fr>
2021-10-24 13:14:09 -0700

    added website with single html file to storage bucket definition

--- bucket.tf
+++ bucket.tf
@@ -5,9 +5,21 @@ provider "google" {
 }
 
 # Create a GCS Bucket
-resource "google_storage_bucket" "my_bucket" {
+resource "google_storage_bucket" "static_site" {
   name                        = var.bucket_name
   location                    = var.region
   force_destroy               = true
   uniform_bucket_level_access = true
+
+    website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  cors {
+    origin          = ["*"]
+    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+
 }
