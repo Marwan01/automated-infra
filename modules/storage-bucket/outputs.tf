cfbc92fd78b5f79eaaa3b4c9aa4353478d7eb3f5
Marouen Helali <marwanest@live.fr>
2021-10-24 16:55:17 -0700

    cleanup output values

--- modules/storage-bucket/outputs.tf
+++ modules/storage-bucket/outputs.tf
@@ -1,48 +1,9 @@
-output "bucket" {
-  description = "Bucket resource (for single use)."
-  value       = local.first_bucket
-}
-
 output "name" {
-  description = "Bucket name (for single use)."
-  value       = local.first_bucket.name
+  description = "Bucket name"
+  value       = google_storage_bucket.static_site.name
 }
 
 output "url" {
-  description = "Bucket URL (for single use)."
-  value       = local.first_bucket.url
-}
-
-output "buckets" {
-  description = "Bucket resources as list."
-  value       = local.buckets_list
-}
-
-output "buckets_map" {
-  description = "Bucket resources by name."
-  value       = google_storage_bucket.buckets
-}
-
-output "names" {
-  description = "Bucket names."
-  value = { for name, bucket in google_storage_bucket.buckets :
-    name => bucket.name
-  }
-}
-
-output "urls" {
-  description = "Bucket URLs."
-  value = { for name, bucket in google_storage_bucket.buckets :
-    name => bucket.url
-  }
-}
-
-output "names_list" {
-  description = "List of bucket names."
-  value       = local.buckets_list[*].name
-}
-
-output "urls_list" {
-  description = "List of bucket URLs."
-  value       = local.buckets_list[*].url
+  description = "Bucket URL"
+  value       = google_storage_bucket.static_site.url
 }
