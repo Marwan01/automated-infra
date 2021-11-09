9323040570a3f08197dfd209c588f66e9dcb93df
Marouen Helali <marwanest@live.fr>
2021-11-09 10:54:15 -0800

    added hard coded storage bucket for site src

--- modules/storage-bucket/main.tf
+++ modules/storage-bucket/main.tf
@@ -1,17 +1,3 @@
-data "google_iam_policy" "viewer" {
-  binding {
-    role = "roles/storage.objectViewer"
-    members = [
-      "allUsers",
-    ]
-  }
-}
-
-resource "google_storage_bucket_iam_policy" "editor" {
-  bucket      = google_storage_bucket.static_site.name
-  policy_data = data.google_iam_policy.viewer.policy_data
-}
-
 resource "google_storage_bucket" "static_site" {
   name          = var.bucket_name
   location      = var.region
@@ -27,4 +13,24 @@ resource "google_storage_bucket" "static_site" {
     response_header = ["*"]
     max_age_seconds = 3600
   }
+}
+
+data "google_iam_policy" "viewer" {
+  binding {
+    role = "roles/storage.objectViewer"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_storage_bucket_iam_policy" "editor" {
+  bucket      = google_storage_bucket.static_site.name
+  policy_data = data.google_iam_policy.viewer.policy_data
+}
+
+resource "google_storage_bucket_object" "static_site_src" {
+  name   = "index.html"
+  source = "public/index.html"
+  bucket = google_storage_bucket.static_site.name
 }
